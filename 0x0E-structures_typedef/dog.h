851d32b5d7262910b478fe75bd369f83a4da044b
hnartey <hanselnartey@gmail.com>
2023-08-14 11:02:21 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,20 +15,6 @@ int main(void)
     my_dog.owner = "Bob";
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
-};
-
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
-#endif
+}
 
 
